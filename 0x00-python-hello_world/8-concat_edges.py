fa2bad14866ac337e2aeede6d95ff8e30c804300
Taofeek Ajibade <cleargoal.ng@gmail.com>
2023-11-27 18:51:35 +0100

    edited source code

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -2,4 +2,4 @@
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
 str = str[str.find("object-oriented programming"):].split(' ', 2)[0] + ' with Python'
-print(str, end='\n')
+print(str)
