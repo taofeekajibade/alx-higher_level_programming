7b6ec008cb7059fe32d823422594acf5c05bede4
Taofeek Ajibade <cleargoal.ng@gmail.com>
2023-11-28 17:54:26 +0100

    edited for pycodestyle

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-# Author - Oluwadamilola Olaniyi
 for letter in range(97, 123):
     if chr(letter) != 'q' and chr(letter) != 'e':
         print("{}".format(chr(letter)), end="")
