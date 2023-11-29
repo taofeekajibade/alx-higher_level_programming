0c8850dea4acc29cf767e4590b82791dcdb6f4ee
Taofeek Ajibade <cleargoal.ng@gmail.com>
2023-11-29 19:58:56 +0100

    added advanced tasks

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+#Author Taofeek Ajibade
+
+def remove_char_at(str, n):
+	if n < 0:
+		return (str)
+	return (str[:n] + str[n+1:])
+
---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+#Author - Taofeek Ajibade
+
+def magic_calculation(a, b, c):
+	"""Match bytecode provided by Holberton school."""
+	if a < b:
+		return (c)
+	if c > b:
+		return (a + b)
+	return (a*b - c)
+
