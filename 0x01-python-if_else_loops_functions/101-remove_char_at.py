d2e4e2afda02e56da61995828df5e0d69d67dd94
Taofeek Ajibade <cleargoal.ng@gmail.com>
2023-11-30 02:48:07 +0100

    pefect pycodestyle!

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -3,7 +3,6 @@
 
 def remove_char_at(str, n):
 
-
-	if (n < 0):
-		return (str)
-	return (str[:n] + str[n+1:])
+    if (n < 0):
+        return (str)
+    return (str[:n] + str[n+1:])
--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -2,9 +2,9 @@
 # Author - Taofeek Ajibade
 
 def magic_calculation(a, b, c):
-	""" Match bytecode provided by Holberton"""
-	if a < b:
-		return (c)
-	if c > b:
-		return (a + b)
-	return (a*b - c)
+    """ Match bytecode provided by Holberton"""
+    if a < b:
+        return (c)
+    if c > b:
+        return (a + b)
+    return (a*b - c)
