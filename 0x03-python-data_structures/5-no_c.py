538c63b646b96f4fb43fa2f21a94644f8c799871
Taofeek Ajibade <cleargoal.ng@gmail.com>
2023-12-04 20:19:01 +0100

    edited codes for pycodestyle

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,7 +2,7 @@
 def no_c(my_string):
     new_str = ""
     for i in my_string:
-        if i is not 'c' and i is not 'C':
+        if i != 'c' and i != 'C':
             new_str += i
     return (new_str)
 # return my_string.translate({ord(c): None for c in "cC"})
