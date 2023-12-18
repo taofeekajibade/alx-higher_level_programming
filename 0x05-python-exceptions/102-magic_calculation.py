818b012be2a61ab1646fd4b58438cd856d5faae8
Taofeek Ajibade <cleargoal.ng@gmail.com>
2023-12-18 20:50:26 +0100

    added advanced task 102-

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+# A python function that mimicks a sample codeprovided in task 9 (102)
+# of ALXSE questions on Python Exceptions
+
+def magic_calculation(a, b):
+    result = 0
+    return result
