ac37a044bd4f600aeedaab2273c7523af2387c41
Taofeek Ajibade <cleargoal.ng@gmail.com>
2023-12-18 19:14:40 +0100

    attempted task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+# A simple function that prints x elements of a list.
+
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    try:
+        while count < x:
+            print(my_list[count], end=' ')
+            count += 1
+    except IndexError:
+        pass
+    finally:
+        print()
+    return count
